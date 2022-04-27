69130e9a0ea723a5ba9a2aea6bc488a3bacdb07a
ellenassis <ellenassis8@gmail.com>
2022-04-27 17:10:43 -0300

    feat: add page Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import SyncLoader from "react-spinners/SyncLoader";
+
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Works from '../components/Works';
+import Contact from '../components/Contact';
+
+import "../styles/global.scss";
+
+function Home() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+  return (
+    <div>
+      {loading ? ( // tela de carregamento
+        <span className="spinner">
+          <SyncLoader size={30} color={"#ed217c"} loading={loading} />
+        </span>
+      ) : (
+        <>
+          <Header />
+          <Hero />
+          <About />
+          <Works />
+          <Contact />
+          <footer>
+            <p>&copy; 2022 | Ellen Penteado de Assis</p>
+          </footer>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Home;
