0659bff1ebfd63571e4e4ed4fab5d41435cf112e
ellenassis <ellenassis8@gmail.com>
2022-04-27 14:26:58 -0300

    fix: responsive

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,22 +3,29 @@ import React from "react";
 import "./styles.scss";
 
 import Email from "../../images/email.png";
+import Whatsapp from "../../images/whatsapp.png";
 
 function Contact() {
   return (
     <section id="contact" className="contact">
       <div className="container">
-        <div className="contact-info">
+        <div className="contact-content">
           <h3>Entre em contato</h3>
           <p>
             Estou procurando por novas oportunidades. Qualquer dúvida ou se
             deseja bater um papo comigo, ficarei feliz em receber e irei
             retornar assim que possível!
           </p>
-          <div className="email">
+          <div className="contact-info">
             <a href="mailto:ellenassis8@gmail.com">
               <img src={Email} alt="" />
               ellenassis8@gmail.com
+            </a>
+          </div>
+          <div className="contact-info">
+            <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5511970516022" target="_blank">
+              <img src={Whatsapp} alt="" />
+              (11) 97051-6022
             </a>
           </div>
         </div>
