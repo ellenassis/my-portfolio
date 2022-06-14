80eb66aaf7a216ea055eedfd4f7c549fea4b3b25
ellenassis <ellenassis8@gmail.com>
2022-06-14 16:47:46 -0300

    feat: change contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -12,8 +12,7 @@ function Contact() {
         <div className="contact-content">
           <h3>Entre em contato</h3>
           <p>
-            Estou procurando por novas oportunidades. Qualquer dúvida ou se
-            deseja bater um papo comigo, ficarei feliz em receber e irei
+            Se deseja bater um papo comigo ou dar seu feedback, ficarei feliz em receber e irei
             retornar assim que possível!
           </p>
           <div className="contact-info">
